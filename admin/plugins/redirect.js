304dad9e024b44d23ee7fb4c2e4221df3ef93483
fuurin <komatsuemgbr0624@gmail.com>
2020-10-22 05:02:22 +0900

    redirect logic

--- admin/plugins/redirect.js
+++ admin/plugins/redirect.js
@@ -1,11 +1,13 @@
 export default function ({ app, store, route }) {
-  if (app.$cookies.get(store.state.auth.SESSION_KEY)) {
-    if (route.path === '/sign_in') {
-      alert('既にログインしています。');
-      window.location.href = '/'
-      return new Promise(() => {});
-    }
-  } else if(route.path !== '/sign_in') {
+  const session = app.$cookies.get(store.state.auth.SESSION_KEY)
+
+  if (session && route.path === '/sign_in') {
+    alert('既にログインしています。');
+    window.location.href = '/'
+    return new Promise(() => {});
+  }
+
+  if(!session && route.path !== '/sign_in') {
     alert('ログインして下さい。');
     window.location.href = '/sign_in';
     return new Promise(() => {});
--- admin/store/index.js
+++ admin/store/index.js
@@ -2,12 +2,16 @@ const DEFAULT_PAGE_TITLE = 'クイズ大会アプリ 管理画面';
 
 export const state = () => {
   return {
-    page_title: DEFAULT_PAGE_TITLE
+    page_title: DEFAULT_PAGE_TITLE,
+    snackbar_text: ''
   }
 }
 
 export const mutations = {
   page_title(state, page_title) {
     state.page_title = page_title;
+  },
+  snackbar(state, text) {
+    state.snackbar_text = text;
   }
 }
